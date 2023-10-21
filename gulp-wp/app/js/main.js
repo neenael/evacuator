3081e5dce08d06b2ad89ef722204db43d9cdfb85
neenael <109112345+neenael@users.noreply.github.com>
2023-10-21 22:27:43 +0300

    Added css styles for header, hero, service, additional services, info, faq sections

---
+++ gulp-wp/app/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let acc = new Accordion('.accordion__list', {
+    duration: 300,
+    elementClass: 'accordion__item',
+    triggerClass: 'accordion__top',
+    panelClass: 'accordion__bottom',
+    showMultiple: false,
+      collapse: true
+  });
+  })
+
+import 'slick-carousel';
+
+$(document).ready(function() {
+  $('.meow').slick();
+});
