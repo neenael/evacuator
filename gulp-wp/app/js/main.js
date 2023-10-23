3815200716bd153fd1828e91e3a173f9104398a9
neenael <109112345+neenael@users.noreply.github.com>
2023-10-23 20:37:36 +0300

    added adaptability

--- gulp-wp/app/js/main.js
+++ gulp-wp/app/js/main.js
@@ -8,3 +8,33 @@ document.addEventListener("DOMContentLoaded", function () {
     collapse: true,
   });
 });
+
+const burgerBtn = document.getElementById("burger");
+const burgerMenu = document.getElementById("bugrer-menu");
+const body = document.body;
+
+function openMenu() {
+  burgerBtn.classList.add("active");
+  burgerMenu.classList.add("active");
+  body.classList.add("stop-scroll");
+}
+
+function closeMenu() {
+  burgerBtn.classList.remove("active");
+  burgerMenu.classList.remove("active");
+  body.classList.remove("stop-scroll");
+}
+
+burgerBtn.addEventListener("click", function () {
+  if (burgerBtn.classList.contains("active")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+document.querySelectorAll(".header__nav--burger__a").forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    closeMenu();
+  });
+});
